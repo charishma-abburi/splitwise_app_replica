844e783f4b8b8ce3c70aa7fdf0f50d042d01aa1b
Hridyanshu Kumar <hridyanshukumar@gmail.com>
2023-04-30 14:36:09 +0530

    Sign in UI and bug fixed

--- lib/src/authenticate/register.dart
+++ lib/src/authenticate/register.dart
@@ -19,6 +19,7 @@ class _RegisterState extends State<Register> {
   String password = '';
   String error = '';
   String name = '';
+  String phone = '';
   bool _obscureText = true;
   final _formKey = GlobalKey<FormState>();
   @override
@@ -174,16 +175,10 @@ class _RegisterState extends State<Register> {
                                       )
                                   )
                               ),
-                              validator: (val) {
-                                if (val == null) {
-                                  return "Enter an non null email";
-                                } else {
-                                  return val.isEmpty ? "Enter an email" : null;
-                                }
-                              },
+
                               onChanged: (val) {
                                 setState(() {
-                                  email = val;
+                                  phone = val;
                                 });
                               },
                             ),
@@ -269,125 +264,5 @@ class _RegisterState extends State<Register> {
           )
       );
   }
-  // @override
-  // Widget build(BuildContext context) {
-  //   return Scaffold(
-  //       appBar: AppBar(
-  //           backgroundColor: Colors.brown[400],
-  //           elevation: 0.0,
-  //           title: const Text('Sign Up to split-wise'),
-  //       ),
-  //       body: Container(
-  //           padding: const EdgeInsets.symmetric(vertical: 20, horizontal: 50),
-  //           child: Form(
-  //               key: _formKey,
-  //               child: Column(
-  //                 children: [
-  //                   const SizedBox(
-  //                     height: 20.0,
-  //                   ),
-  //                   TextFormField(
-  //                     decoration: const InputDecoration(
-  //                       labelText: 'Email',
-  //                     ),
-  //                     validator: (val) {
-  //                       if (val == null) {
-  //                         return "Enter an non null email";
-  //                       } else {
-  //                         return val.isEmpty ? "Enter an email" : null;
-  //                       }
-  //                     },
-  //                     onChanged: (val) {
-  //                       setState(() {
-  //                         email = val;
-  //                       });
-  //                     },
-  //                   ),
-  //                   const SizedBox(
-  //                     height: 20.0,
-  //                   ),
-  //                   TextFormField(
-  //                     decoration: const InputDecoration(
-  //                       labelText: 'Name',
-  //                     ),
-  //                     validator: (val) {
-  //                       if (val == null) {
-  //                         return "Enter an non null Name";
-  //                       } else {
-  //                         return val.isEmpty ? "Enter a Name" : null;
-  //                       }
-  //                     },
-  //                     onChanged: (val) {
-  //                       setState(() {
-  //                         name = val;
-  //                       });
-  //                     },
-  //                   ),
-  //                   const SizedBox(
-  //                     height: 20.0,
-  //                   ),
-  //                   TextFormField(
-  //                     decoration: const InputDecoration(
-  //                       labelText: 'Password',
-  //                     ),
-  //                     validator: (val) {
-  //                       if (val == null) {
-  //                         return "Enter a non null password of minimum 6 chars";
-  //                       } else {
-  //                         return val.length < 6
-  //                             ? "Enter a password of minimum 6 chars"
-  //                             : null;
-  //                       }
-  //                     },
-  //                     obscureText: true,
-  //                     onChanged: (val) {
-  //                       setState(() {
-  //                         password = val;
-  //                       });
-  //                     },
-  //                   ),
-  //                   const SizedBox(
-  //                     height: 20,
-  //                   ),
-  //                   ElevatedButton(
-  //                       onPressed: () async {
-  //                         if (_formKey.currentState!.validate()) {
-  //                           dynamic result = await _auth
-  //                               .registerWithEmailAndPassword(email, password);
-  //                           if (result == null) {
-  //                             setState(() {
-  //                               error = 'Enter a valid email';
-  //                             });
-  //                           } else {
-  //                             Navigator.pop(context);
-  //                             print(result.uid);
-  //                             await _db.CreateUser(
-  //                                 result.uid, email, name);
-  //                             setState(() {
-  //                               error = '';
-  //                             });
-  //                           }
-  //                         }
-  //                       },
-  //                       style: ButtonStyle(
-  //                         backgroundColor:
-  //                             MaterialStateProperty.all(Colors.greenAccent),
-  //                       ),
-  //                       child: const Text(
-  //                         'Sign Up',
-  //                         style: TextStyle(color: Colors.black),
-  //                       )),
-  //                   const SizedBox(
-  //                     height: 30,
-  //                   ),
-  //                   Text(
-  //                     error,
-  //                     style: const TextStyle(color: Colors.red, fontSize: 15),
-  //                   )
-  //                 ],
-  //               )
-  //           )
-  //       ),
-  //   );
-  // }
+
 }
