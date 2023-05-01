d13c8d758b8355d718ec821f175f83f7f083561e
Hridyanshu Kumar <hridyanshukumar@gmail.com>
2023-05-01 15:00:39 +0530

    Account page UI and Added reset password

--- lib/src/account_page.dart
+++ lib/src/account_page.dart
@@ -1,3 +1,4 @@
+import 'package:fluttertoast/fluttertoast.dart';
 import 'package:splitwise_app_replica/constants.dart';
 import 'package:splitwise_app_replica/services/database.dart';
 import 'package:firebase_auth/firebase_auth.dart';
@@ -76,72 +77,112 @@ class _AccountPageState extends State<AccountPage> {
                   ),
                   const SizedBox(height: 20),
                   Center(
-                    child: ElevatedButton(
-                      // style: ButtonStyle(backgroundColor:Color.fromRGBO(76, 187, 155, 1), ),
-                      style: ButtonStyle(
-                        backgroundColor: MaterialStateProperty.all<Color>(
-                          Color.fromRGBO(76, 187, 155, 1),
+                    child: Container(
+                      height: 80.0,
+                      width: double.infinity,
+                      child: TextButton(
+                        // style: ButtonStyle(backgroundColor:Color.fromRGBO(76, 187, 155, 1), ),
+                        style: TextButton.styleFrom(
+                            alignment: Alignment.centerLeft,
+                            foregroundColor: Colors.grey
                         ),
-                      ),
-                      onPressed: () {
-                        showDialog(
-                          context: context,
-                          builder: (BuildContext context) {
-                            return AlertDialog(
-                              title: const Text('Set up UPI ID'),
-                              content: TextFormField(
-                                decoration: const InputDecoration(
-                                  hintText: 'Enter your UPI ID',
+                        onPressed: () {
+                          showDialog(
+                            context: context,
+                            builder: (BuildContext context) {
+                              return AlertDialog(
+                                title: const Text('Set up UPI ID', style: TextStyle(fontWeight: FontWeight.w300),),
+                                content: TextFormField(
+                                  decoration: const InputDecoration(
+                                    hintText: 'Enter your UPI ID',
+                                  ),
+                                  onChanged: (value) {
+                                    upiID = value;
+                                    // Store the entered UPI ID in a local variable or state variable
+                                  },
                                 ),
-                                onChanged: (value) {
-                                  upiID = value;
-                                  // Store the entered UPI ID in a local variable or state variable
-                                },
-                              ),
-                              actions: [
-                                ElevatedButton(
-                                  style: ButtonStyle(
-                                    backgroundColor:
-                                        MaterialStateProperty.all<Color>(
-                                      Color.fromRGBO(76, 187, 155, 1),
+                                actions: [
+                                  ElevatedButton(
+                                    style: ButtonStyle(
+                                      backgroundColor:
+                                          MaterialStateProperty.all<Color>(
+                                        Color.fromRGBO(76, 187, 155, 1),
+                                      ),
+                                    ),
+                                    onPressed: () async {
+                                      // Save the entered UPI ID to the Firestore database
+                                      await _db.addUPI(uid, upiID);
+                                      Navigator.pop(context);
+                                    },
+                                    child: const Text('Save'),
+                                  ),
+                                  TextButton(
+
+
+                                    onPressed: () {
+                                      Navigator.pop(context);
+                                    },
+                                    child: const Text('Cancel',
+                                    style: TextStyle(
+                                      color:  Color.fromRGBO(76, 187, 155, 1),
+                                    ),
                                     ),
                                   ),
-                                  onPressed: () async {
-                                    // Save the entered UPI ID to the Firestore database
-                                    await _db.addUPI(uid, upiID);
-                                    Navigator.pop(context);
-                                  },
-                                  child: const Text('Save'),
-                                ),
-                                TextButton(
-                                
-                
-                                  onPressed: () {
-                                    Navigator.pop(context);
-                                  },
-                                  child: const Text('Cancel',
-                                  style: TextStyle(
-                                    color:  Color.fromRGBO(76, 187, 155, 1),
-                                  ),
-                                  ),
-                                ),
-                              ],
-                            );
-                          },
-                        );
-                      },
-                      child: const Text('Set up UPI ID',
-                          style: TextStyle(fontSize: 24)),
+                                ],
+                              );
+                            },
+                          );
+                        },
+                        child: const Text('Set up UPI ID',
+                            style: TextStyle(
+                                color: Colors.black54,
+                                fontSize: 20,
+                                fontWeight: FontWeight.w400
+                            )
+                        ),
+                      ),
                     ),
                   ),
-                  const SizedBox(
-                    height: 20,
+                  
+                  Container(
+                    height: 80.0,
+                    width: double.infinity,
+                    child: TextButton(
+                      onPressed: () async {
+                        await FirebaseAuth.instance.sendPasswordResetEmail(email:email!)
+                            .then((value) => {
+                              Fluttertoast.showToast(
+                                  msg: "Sent a reset link to your email",
+                                backgroundColor: colorTheme,
+                                textColor: Colors.white
+                              )
+                        })
+                            .onError((error, stackTrace) => {
+                          Fluttertoast.showToast(msg: "No such user")
+                        });
+                      },
+                      style: TextButton.styleFrom(
+                          alignment: Alignment.centerLeft,
+                        foregroundColor: Colors.grey
+                      ),
+                      child: const Text(
+                        "Reset your password",
+                        style: TextStyle(
+                          color: Colors.black54,
+                          fontSize: 20,
+                          fontWeight: FontWeight.w400
+                        ),
+                      ),
+                    ),
                   ),
+
                   Center(
                     child: Container(
-                      // width: double.infinity,
+                      height: 80.0,
+                      width: double.infinity,
                       child: TextButton.icon(
                         style: TextButton.styleFrom(
+                          foregroundColor: Colors.grey,
                             alignment: Alignment.centerLeft),
                         onPressed: () async {
                           await _auth.signOut();
